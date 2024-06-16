bf8d365ff28a0ad992d34f0c85f36865e99f8ecf
Frost-ZX <frost-zx@foxmail.com>
2024-06-16 19:19:45 +0800

    feat: 支持使用指令退出程序

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,27 @@
 import { startHTTPServer } from './http.js';
 import { startRconClient } from './rcon.js';
 
+const PREFIX = '[process]';
+
 console.clear();
 
 startHTTPServer();
 startRconClient();
+
+// 监听输入指令
+process.stdin.addListener('data', function (data) {
+
+  let command = String(data).replace(/[\r\n]/g, '');
+
+  console.log(PREFIX, '收到指令 ->', command);
+
+  switch (command) {
+    case 'q':
+      console.warn(PREFIX, '退出程序');
+      process.exit(0);
+    default:
+      console.error(PREFIX, '指令不存在');
+      break;
+  }
+
+});
